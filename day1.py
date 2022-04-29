99114f06472d3aeb31de4a1a7160ca18b908d9f2
Katie Shannon <katieshannon@Katies-MacBook-Pro.local>
2022-04-29 13:13:39 -0700

    day 4 and 5, refactoring and Linting some other code

--- day1.py
+++ day1.py
@@ -2,6 +2,7 @@
 
 from get_input import get_input_for_day
 
+
 def part_one(input_data):
     previous_distance = 10000000
     larger_distance_count = 0
@@ -9,7 +10,8 @@ def part_one(input_data):
         if distance > previous_distance:
             larger_distance_count += 1
         previous_distance = distance
-    print(larger_distance_count)
+    return larger_distance_count
+
 
 def part_two(input_data):
     larger_sum_count = 0
@@ -19,12 +21,12 @@ def part_two(input_data):
         if distance_sum > previous_sum:
             larger_sum_count += 1
         previous_sum = distance_sum
-    print(larger_sum_count)
-
-
+    return larger_sum_count
 
 
 if __name__ == "__main__":
     input_data = [int(x) for x in get_input_for_day(1)]
-    part_one(input_data)
-    part_two(input_data)
+    part_one_answer = part_one(input_data)
+    part_two_answer = part_two(input_data)
+    print(f'part_one_answer: {part_one_answer}')
+    print(f'part_two_answer: {part_two_answer}')
